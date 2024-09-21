faae93f0919a1346696125f7abdeaac8868f131c
MAZE <remvze@proton.me>
2024-09-21 21:32:04 +0330

    chore: update the domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), sitemap()],
-  site: 'https://calmness.app',
+  site: 'https://calmness.mvze.net',
 });
